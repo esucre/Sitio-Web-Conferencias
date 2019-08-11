7c670287bcca0779c3228e9af83dc4d59ae5c72b
emilio sucre <emilio.sucre@utp.ac.pa>
2019-08-11 10:12:43 -0500

    Animacion al programa de conferencias

--- js/main.js
+++ js/main.js
@@ -32,6 +32,7 @@ var regalo =document.getElementById('regalo');
         let pase_dia = document.getElementById('pase_dia');
         let pase_dosdias = document.getElementById('pase_dosdias');
         let pase_completo = document.getElementById('pase_completo');
+        let caja = document.getElementById('caja');
 
         // Botones y divs
         let calcular = document.getElementById('calcular');
@@ -39,6 +40,7 @@ var regalo =document.getElementById('regalo');
         let botonRegistro = document.getElementById('btnRegistro');
         let lista_productos = document.getElementById('lista-productos');
         let suma = document.getElementById('suma-total');
+        
 
         //Extras
         let etiquetas = document.getElementById('etiqueta');
@@ -135,26 +137,52 @@ var regalo =document.getElementById('regalo');
 
             let diasElegidos = [];
 
+
             if(boletosDia >=1){diasElegidos.push('viernes')};
             if(boletos_dosdias >=1){diasElegidos.push('viernes','sabado')} ;
             if(boletoCompleto >=1){diasElegidos.push('viernes','sabado','domingo')} ;
 
+            
             for(let i = 0; i < diasElegidos.length;i++){
                 if(diasElegidos[i] !== ''){document.getElementById(diasElegidos[i]).style.display ='block';}
-                
-                
             }
 
-            //boletosDia =
-            //boletos_dosdias = 
-           // boletoCompleto = 
 
 
-            console.log(boletosDia);
-            console.log(boletos_dosdias);
-            console.log(boletoCompleto);
-            console.log(diasElegidos);
+            // console.log(boletosDia);
+            // console.log(boletos_dosdias);
+            // console.log(boletoCompleto);
+            // console.log(diasElegidos);
         }
 
     });//DOM Content
 })();//Function
+
+$(function(){
+
+//Programa de Conferencias
+$('.programa-evento .info-curso:first').show();
+$('.menu-programa a:first').addClass('activo');
+$('.menu-programa a').on('click', function(){
+    $('.menu-programa a').removeClass('activo');
+    $(this).addClass('activo');
+    $('.ocultar').hide();
+    var enlace = $(this).attr('href');
+    $(enlace).fadeIn(1000);
+    return false;
+    });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
